7c3080ddfbb5a74df8ea6dcd1532b011c3a20b31
mybffjustin <jhoang1@madisoncollege.edu>
2022-02-20 17:42:12 -0600

    Update pom.xml

---
+++ src/main/java/org/justinhoang/persistence/SessionFactory.java
@@ -0,0 +1,28 @@
+package org.justinhoang.persistence;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class SessionFactory {
+
+
+
+    protected void setUp() throws Exception {
+        // A SessionFactory is set up once for an application!
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure() // configures settings from hibernate.cfg.xml
+                .build();
+        try {
+            sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
+        }
+        catch (Exception e) {
+            // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
+            // so destroy it manually.
+            StandardServiceRegistryBuilder.destroy( registry );
+        }
+    }
+
+
+}
