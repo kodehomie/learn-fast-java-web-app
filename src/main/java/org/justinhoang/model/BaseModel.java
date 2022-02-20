66321dd1dc55d0932c11b8f707109257cd8e4d6f
mybffjustin <jhoang1@madisoncollege.edu>
2022-02-20 17:32:15 -0600

    Add Event.java

--- src/main/java/org/justinhoang/model/BaseModel.java
+++ src/main/java/org/justinhoang/model/BaseModel.java
@@ -13,13 +13,13 @@ public class BaseModel implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "native")
     @GenericGenerator(name = "native", strategy = "native")
-    private Integer id;
+    private Long id;
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
